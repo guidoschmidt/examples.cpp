09843999e117b692fbf2d2b007d1a9b077e2ae40
Guido Schmidt <git@guidoschmidt.cc>
2020-08-17 21:50:18 +0200

    EDIT/std/system/system.cpp: rtrim + get worktree changes

--- std/system/system.cpp
+++ std/system/system.cpp
@@ -5,24 +5,29 @@
 using std::cout;
 using std::endl;
 
-int main() {
-  system("git rev-parse --short HEAD");
+void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
 
-  FILE *file_ptr, *out_file;
+int main() {
+  // system("git rev-parse --short HEAD");
+
+  FILE *file_ptr;
   char var[256];
   file_ptr = popen("git rev-parse --short HEAD", "r");
-  // while (fgets(var, sizeof(var), file_ptr) != NULL) {
-  //   cout << var << endl;
-  // }
+  fgets(var, sizeof(var), file_ptr);
   pclose(file_ptr);
   std::string hash = std::string(var);
-  cout << var << endl;
+  rtrim(hash);
+  cout << "Git hash: " << hash << endl;
 
   // Get git status
-
+  char changes[10];
   file_ptr = popen("git diff HEAD | wc -l", "r");
-  while (fgets(var, sizeof(var), file_ptr) != NULL) {
-    cout << std::stoi(var) << endl;
-  }
+  fgets(changes, sizeof(changes), file_ptr);
+  int change_count = std::stoi(changes);
+  cout << change_count << endl;
   pclose(file_ptr);
 }
